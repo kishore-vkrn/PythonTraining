b16e1cb4ae01993c34f7db1540d84f52c51ea0d7
root <root@ec2-54-89-72-254.compute-1.amazonaws.com>
2016-07-11 12:47:02 +0000

    reverting

--- xml_changer/apend.py
+++
@@ -1,7 +0,0 @@
-import sys
-import xml.etree.ElementTree as ET
-
-class Users(object):
-    def __init__(self, users=None):
-        self.doc = ET.parse("users.xml")
-        self.root = self.doc.getroot()
--- xml_changer/init.groovy.d/2_cloud.groovy
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-println "Executing cloud changes"
-def exitValue = "appendtoxml.sh".execute().exitValue()
-if(!exitValue) {
-	println "REview your config settings"
-}
-
